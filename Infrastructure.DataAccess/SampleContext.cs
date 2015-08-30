cbff037689b2f6178c0eabc7a7d05da3f9b97701
snebjorn <eskildd@gmail.com>
2015-08-30 18:02:24 +0200

    Moved database initializer to dbcontext. Set DateTime type to datetime2 SQL server type.

--- Infrastructure.DataAccess/SampleContext.cs
+++ Infrastructure.DataAccess/SampleContext.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using Core.DomainModel;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace Infrastructure.Data
 {
@@ -11,7 +12,38 @@ namespace Infrastructure.Data
         public SampleContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            // Database.SetInitializer<SampleContext>(null);
+            // The database initializer will create the database and the specified tables. 
+            // If you're using an existing database with code first, don't execute any logic at all.
 
+            // Database.SetInitializer(new CreateDatabaseIfNotExists<SampleContext>()); // Default, will run if nothing is done.
+            // The default option. When the application runs the first time, entity framework will create
+            // a code first database if it does not already exist. If the database exists and you have done modifications
+            // this will throw an InvalidOperationException.
+
+            // Database.SetInitializer(new DropCreateDatabaseAlways<SampleContext>());
+            // This option, as the name suggests, will always drop and recreate the database when the application runs the first time.
+            // All tables will be deleted as the database is dropped.
+
+            // Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SampleContext>());
+            // This option will drop and recreate the database if there are any changes to the model.
+
+            // Alternatively, you can create your own initializer and pass it as the argument.
+            // The class will need to implement one of the above in order to inherit some behaviour.
+            // These Initializers has a custom seeding function, that will populate the database instance
+            // with some fake data that can be used right away.
+            if (Database.Exists("DefaultConnection"))
+            {
+                // Runs if a database already exists. It drops the database, recreates it and seeds with some fake data.
+                Database.SetInitializer(new ChangeSampleSeedInitializer());
+            }
+            else 
+            {
+                // This initilizer will run if there are no database.
+                // Usually this is on the first run, or if the database was deleted.
+                // Seeds with some fake data.
+                Database.SetInitializer(new CreateSampleSeedInitializer());
+            }
         }
 
         public static SampleContext Create()
@@ -27,10 +59,11 @@ namespace Infrastructure.Data
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>().Property(s => s.CreatedOn).HasColumnType("datetime2");
-            modelBuilder.Entity<Student>().Property(s => s.ModifiedOn).HasColumnType("datetime2");
+            // The DateTime type in .NET has the same range and precision as datetime2 in SQL Server.
+            // Configure DateTime type to use SQL server datetime2 instead.
+            modelBuilder.Properties<DateTime>().Configure(c => c.HasColumnType("datetime2"));
 
-            // Consider setting maxlength on string properties, 
+            // Consider setting maxlength on string properties.
             // http://dba.stackexchange.com/questions/48408/ef-code-first-uses-nvarcharmax-for-all-strings-will-this-hurt-query-performan
             modelBuilder.Entity<Student>().Property(s => s.Name).HasMaxLength(50);
             modelBuilder.Entity<Course>().Property(s => s.Name).HasMaxLength(50);
