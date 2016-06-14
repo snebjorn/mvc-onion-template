768a2e79df80b6a14a5ccc12f7d6ff3eeeaa7455
Lasse Fisker <lnf@it-minds.dk>
2016-06-14 12:04:23 +0200

    added support for attribute routing

--- Presentation.Web/App_Start/RouteConfig.cs
+++ Presentation.Web/App_Start/RouteConfig.cs
@@ -9,6 +9,8 @@ namespace Presentation.Web.App_Start
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
